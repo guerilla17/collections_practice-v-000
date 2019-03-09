c52a57c2f5736cf28b5516bc7873615e89ddadd7
Gigi M <mmgamao1@gmail.com>
2019-03-09 17:48:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,15 +2,15 @@ def sort_array_asc(array)
   array.sort
 end
 
-def sort_array_desc(array)
-  array.sort
-end
+//def sort_array_desc(array)
+  //array.sort
+//end
 
 def sort_array_char_count
   array.sort.length  
 end
 
-def swap_elements(array)
+//def swap_elements(array)
   
   
 def reverse_array(array)
